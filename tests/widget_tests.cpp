441bd857c6ee0d775a03a191c5dfe695563a158c
Robin Gustafsson <robin.gustafsson14@gmail.com>
2019-06-20 00:12:32 +0200

    Fixed tests

--- tests/widget_tests.cpp
+++ tests/widget_tests.cpp
@@ -1,62 +1,62 @@
 #define CATCH_CONFIG_MAIN
 #include "catch2/catch.hpp"
 
-#include "ptk/core/transformable.hpp"
+#include "ptk/core/widget.hpp"
 
-TEST_CASE("Transformable Constructors")
+TEST_CASE("Widget Constructors")
 {
     // Testing Constructors with correct data.
     
-    SECTION("Transformable()")
+    SECTION("Widget()")
     {
-        pTK::Transformable t;
+        pTK::Widget t;
         pTK::Vec2<float> correct{0, 0};
         REQUIRE(t.get_size() == correct);
     }
 }
 
-TEST_CASE("Transformable Setters")
+TEST_CASE("Widget Setters")
 {
     // Testing Setters.
     pTK::Vec2<float> correct{10, 20};
     
     SECTION("set_size(float width, float height)")
     {
-        pTK::Transformable t;
+        pTK::Widget t;
         t.set_size(10, 20);
         REQUIRE(t.get_size() == correct);
     }
     
     SECTION("set_size(const Vec2<float>& size)")
     {
-        pTK::Transformable t;
+        pTK::Widget t;
         t.set_size(correct);
         REQUIRE(t.get_size() == correct);
     }
     
     SECTION("set_position(float x, float y)")
     {
-        pTK::Transformable t;
+        pTK::Widget t;
         t.set_position(10, 20);
         REQUIRE(t.get_position() == correct);
     }
     
     SECTION("set_size(const Vec2<float>& size)")
     {
-        pTK::Transformable t;
+        pTK::Widget t;
         t.set_position(correct);
         REQUIRE(t.get_position() == correct);
     }
 }
 
-TEST_CASE("Transformable Getters")
+TEST_CASE("Widget Getters")
 {
     // Testing Getters.
     pTK::Vec2<float> correct{10, 20};
     
     SECTION("const Vec2<float>& get_size() const")
     {
-        pTK::Transformable t;
+        pTK::Widget t;
         t.set_size(correct);
         REQUIRE(t.get_size().x == correct.x);
         REQUIRE(t.get_size().y == correct.y);
@@ -65,7 +65,7 @@ TEST_CASE("Transformable Getters")
     
     SECTION("const Vec2<float>& get_position() const")
     {
-        pTK::Transformable t;
+        pTK::Widget t;
         t.set_position(correct);
         REQUIRE(t.get_position().x == correct.x);
         REQUIRE(t.get_position().y == correct.y);
@@ -73,25 +73,25 @@ TEST_CASE("Transformable Getters")
     }
 }
 
-TEST_CASE("Transformable Copy and Assignment")
+TEST_CASE("Widget Copy and Assignment")
 {
-    // Testing Transformable Copy and Assignment.
+    // Testing Widget Copy and Assignment.
     pTK::Vec2<float> pos{10, 20};
     pTK::Vec2<float> size{15, 45};
-    pTK::Transformable t;
+    pTK::Widget t;
     t.set_size(size);
     t.set_position(pos);
     
     SECTION("Copy")
     {
-        pTK::Transformable tmp = t;
+        pTK::Widget tmp = t;
         REQUIRE(t.get_position() == pos);
         REQUIRE(t.get_size() == size);
     }
     
     SECTION("Assignment")
     {
-        pTK::Transformable tmp;
+        pTK::Widget tmp;
         tmp = t;
         REQUIRE(t.get_position() == pos);
         REQUIRE(t.get_size() == size);
@@ -107,16 +107,16 @@ TEST_CASE("Transformable Copy and Assignment")
     }
 }
 
-TEST_CASE ("Transformable Comparison")
+TEST_CASE ("Widget Comparison")
 {
-    // Testing Transformable Comparison.
-    pTK::Transformable t;
-    pTK::Transformable t1;
-    pTK::Transformable t2;
+    // Testing Widget Comparison.
+    pTK::Widget t;
+    pTK::Widget t1;
+    pTK::Widget t2;
     t2.set_size(42, 14);
-    pTK::Transformable t3;
+    pTK::Widget t3;
     t3.set_position(10, 20);
-    pTK::Transformable t4;
+    pTK::Widget t4;
     t4.set_position(10, 20);
     t4.set_size(42, 14);
     
