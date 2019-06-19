3545e66812aa192af295999169beb99678a79eda
Robin Gustafsson <robin.gustafsson14@gmail.com>
2019-06-19 18:04:02 +0200

    Added documentation for Event

--- include/ptk/core/event.hpp
+++ include/ptk/core/event.hpp
@@ -10,6 +10,10 @@
 
 namespace pTK
 {
+    /** EventType enum class implementation.
+     
+     This enum class is to specify the event type of the event.
+     */
     enum class EventType
     {
         NONE = 0,
@@ -18,6 +22,10 @@ namespace pTK
         MouseButtonPressed, MouseButtonReleased, MouseMoved, MouseScrolled
     };
 
+    /** EventCategory enum class implementation.
+     
+     This enum class is to specify the category type of the event.
+     */
     enum class EventCategory
     {
         NONE = 0,
@@ -26,16 +34,41 @@ namespace pTK
         Mouse
     };
 
+    /** Event class implementation.
+     
+     This class is the base class for events.
+     It specifies which event that occured and cannot be
+     created without specifying the category and type.
+     
+     EventCategory and EventType cannot be changed after the
+     event is created.
+     */
     class Event
     {
     public:
+        /** Constructs Event with default values with t_category
+         and t_type.
+         
+         @param t_category  category of event
+         @param t_type      type of event
+         @return            default initialized Event
+         */
         Event(EventCategory t_category, EventType t_type)
             : m_type{t_type}, m_category{t_category}
         {
         }
         virtual ~Event() = default;
-
+        
+        /** Function for retrieving the type of the event.
+         
+         @return    Type of event
+         */
         EventType get_type() const { return m_type; }
+        
+        /** Function for retrieving the category of the event.
+         
+         @return    Type of event
+         */
         EventCategory get_category() const { return m_category; }
 
     protected:
