d999fcf60eade5b350a51ffa9768a8e685071f3c
Robin Gustafsson <robin.gustafsson14@gmail.com>
2019-06-19 17:53:25 +0200

    Implemented Drawable

---
+++ include/ptk/core/drawable.hpp
@@ -0,0 +1,33 @@
+//
+//  core/drawable.hpp
+//  pTK
+//
+//  Created by Robin Gustafsson on 2019-06-12.
+//
+
+#ifndef PTK_CORE_DRAWABLE_HPP
+#define PTK_CORE_DRAWABLE_HPP
+
+// Skia Headers
+#include "include/core/SkCanvas.h"
+
+namespace pTK
+{
+    /** Drawable class implementation.
+     
+        This class is the base class for drawable widget.
+        Widgets without this class cannot be drawn.
+     */
+    class Drawable
+    {
+    public:
+        virtual ~Drawable() = default;
+        
+        /** Draw function.
+            Function is called when it is time to draw.
+         */
+        virtual void draw() const {}
+    };
+}
+
+#endif // PTK_CORE_DRAWABLE_HPP
