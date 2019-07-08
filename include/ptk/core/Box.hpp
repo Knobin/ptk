466aafeb826a60594ba952c1de2df5fe43a9c28b
Robin Gustafsson <robin.gustafsson14@gmail.com>
2019-07-08 14:03:20 +0200

    Implemented Box class

---
+++ include/ptk/core/Box.hpp
@@ -0,0 +1,187 @@
+//
+//  core/Box.hpp
+//  pTK
+//
+//  Created by Robin Gustafsson on 2019-07-03.
+//
+
+#ifndef PTK_CORE_BOX_HPP
+#define PTK_CORE_BOX_HPP
+
+// Local Headers
+#include "ptk/util/Container.hpp"
+#include "ptk/core/Widget.hpp"
+#include "ptk/util/Color.hpp"
+#include "ptk/Log.hpp"
+
+// C++ Headers
+#include <string>
+#include <memory>
+
+namespace pTK
+{
+    using ContainerType = Container<std::shared_ptr<Widget>>;
+    
+    /** Box class implementation.
+     
+     Base class for handling a collection of Widgets.
+     */
+    class Box : public ContainerType, public Widget
+    {
+    public:
+        Box();
+        virtual ~Box() = default;
+        
+        /** Funtion for adding a widget to the Container.
+         
+         @param widget  widget to add
+         */
+        virtual bool add(const std::shared_ptr<Widget>& widget);
+        
+        /** Funtion for removing a widget in the Container.
+         
+         @param widget  widget to remove
+         */
+        virtual void remove(const std::shared_ptr<Widget>& widget);
+        
+        /** Funtion for finding a widget in the Container.
+         Function will look for a widget with the specified name and it
+         will grab the first with the matching name.
+         
+         Meaning that if two Widgets or more have the same name, only the
+         first added will be found.
+         
+         @param name    name to find
+         @return        const_iterator to widget or end const_iterator
+         */
+        const_iterator find(const std::string& name);
+        
+        /** Funtion for finding a widget in the Container.
+         Function can be used to check if a pointer to Widget exists.
+         
+         @param widget  widget to find
+         @return        const_iterator to widget or end const_iterator
+         */
+        const_iterator find(const Widget* widget);
+        
+        /** Funtion for finding a widget in the Container.
+         
+         @param pos     pos to find
+         @return        const_iterator to widget or end const_iterator
+         */
+        const_iterator find(const Position& pos);
+        
+        /** Funtion for finding a widget in the Container.
+         In reverse order.
+         
+         @param pos     pos to find
+         @return        const_iterator to widget or end const_iterator
+         */
+        const_reverse_iterator rfind(const Position& pos);
+        
+        /** Draw function.
+         Function is called when it is time to draw.
+         
+         Derived from Drawable.
+         @param canvas  canvas to draw to
+         */
+        void onDraw(SkCanvas* canvas) override;
+        
+        // Event handling
+        
+        /** Function for handling when a key is pressed or released.
+         
+         Derived from EventFunctions.
+         
+         @param type    Key event (press or release)
+         @param int     Keycode
+         */
+        bool onKeyEvent(EventType type, int32 keycode) override;
+        
+        /** Function for handling when mouse is hovering.
+         
+         Derived from EventFunctions.
+         
+         @param pos     position
+         */
+        bool onHoverEvent(const Position& pos) override;
+        
+        /** Function for handling when mouse is entering.
+         
+         Derived from EventFunctions.
+         */
+        bool onEnterEvent() override;
+        
+        /** Function for handling when mouse is leaving.
+         
+         Derived from EventFunctions.
+         */
+        bool onLeaveEvent() override;
+        
+        /** Function for handling when mouse is scrolling.
+         
+         Derived from EventFunctions.
+         
+         @param offset     x and y offset
+         */
+        bool onScrollEvent(const Vec2f& offset) override;
+        
+        /** Function for handling when mouse is clicking.
+         
+         Derived from EventFunctions.
+         
+         @param button      which button on mouse triggered the event.
+         @param position    x and y position
+         */
+        bool onClickEvent(MouseButton btn, const Position& pos) override;
+        
+        /** Function for handling when mouse is released.
+         
+         Derived from EventFunctions.
+         
+         @param button      which button on mouse triggered the event.
+         @param position    x and y position
+         */
+        bool onReleaseEvent(MouseButton btn, const Position& pos) override;
+        
+        /** Funtion for setting the background of the Container.
+         
+         @param color   background color
+         */
+        void setBackground(const Color& color);
+        
+        /** Funtion for getting the background of the Container.
+         
+         @return    current background color
+         */
+        const Color& getBackground() const;
+        
+    protected:
+        
+        /** Funtion for drawing all the Widgets in the Container.
+         
+         */
+        void drawWidgets(SkCanvas* canvas);
+        
+        struct layoutData
+        {
+            Widget* widget;
+            Size size;
+            Position pos;
+        };
+        Container<layoutData> m_layoutData;
+        
+    private:
+        // These functions will not be used outside of this class.
+        using ContainerType::insert;
+        using ContainerType::erase;
+        
+        // Variables
+        Color m_background;
+        std::shared_ptr<Widget> m_lastClickedWidget;
+        std::shared_ptr<Widget> m_currentHoverWidget;
+    };
+}
+
+#endif // PTK_CORE_BOX_HPP
+
---
+++ src/core/Box.cpp
@@ -0,0 +1,228 @@
+//
+//  core/Box.hpp
+//  pTK
+//
+//  Created by Robin Gustafsson on 2019-07-03.
+//
+
+// Local Headers
+#include "ptk/core/Box.hpp"
+
+namespace pTK
+{
+    Box::Box()
+    : ContainerType(), Widget(),
+        m_layoutData{}, m_background{0xf5f5f5ff}, m_lastClickedWidget{nullptr}, m_currentHoverWidget{nullptr}
+    {
+        
+    }
+    
+    bool Box::add(const std::shared_ptr<Widget>& widget)
+    {
+        if (ContainerType::find(widget) == end())
+        {
+            insert(widget);
+            m_layoutData.insert({widget.get(), widget->getSize(), widget->getPosition()});
+            widget->setParent(this);
+            return true;
+        }
+        return false;
+    }
+    
+    void Box::remove(const std::shared_ptr<Widget>& widget)
+    {
+        erase(widget);
+        Container<layoutData>::const_iterator it = m_layoutData.findIf([widget](const layoutData& data){
+            if (data.widget == widget.get())
+                return true;
+            return false;
+        });
+        m_layoutData.erase(it);
+    }
+    
+    Box::const_iterator Box::find(const std::string& name)
+    {
+        return findIf([name](const std::shared_ptr<Widget>& widget){
+            if (widget->getName() == name)
+                return true;
+            
+            return false;
+        });
+    }
+    
+    Box::const_iterator Box::find(const Widget* widget)
+    {
+        return findIf([widget](const std::shared_ptr<Widget>& rhs){
+            if (widget == rhs.get())
+                return true;
+            
+            return false;
+        });
+    }
+    
+    Box::const_iterator Box::find(const Position& pos)
+    {
+        return findIf([&pos](const std::shared_ptr<Widget>& widget){
+            Position wPos = widget->getPosition();
+            Size wSize = widget->getSize();
+            if ((wPos.x <= pos.x) && (wPos.x + wSize.width >= pos.x))
+                if ((wPos.y <= pos.y) && (wPos.y + wSize.height >= pos.y))
+                    return true;
+            
+            return false;
+        });
+    }
+    
+    Box::const_reverse_iterator Box::rfind(const Position& pos)
+    {
+        return rfindIf([&pos](const std::shared_ptr<Widget>& widget){
+            Position wPos = widget->getPosition();
+            Size wSize = widget->getSize();
+            if ((wPos.x <= pos.x) && (wPos.x + wSize.width >= pos.x))
+                if ((wPos.y <= pos.y) && (wPos.y + wSize.height >= pos.y))
+                    return true;
+            
+            return false;
+        });
+    }
+    
+    void Box::onDraw(SkCanvas* canvas)
+    {
+        // Set Size and Position
+        SkPoint pos{convertToSkPoint(getPosition())};
+        SkPoint size{convertToSkPoint(getSize())};
+        size += pos; // skia needs the size to be pos+size.
+        
+        // Set Color
+        SkPaint paint;
+        paint.setAntiAlias(true);
+        paint.setARGB(m_background.a, m_background.r, m_background.g, m_background.b);
+        
+        // Draw Rect
+        SkRect rect;
+        rect.set(pos, size);
+        paint.setStyle(SkPaint::kStrokeAndFill_Style);
+        canvas->drawRoundRect(rect, 0, 0, paint);
+        
+        drawWidgets(canvas);
+    }
+    
+    void Box::drawWidgets(SkCanvas* canvas)
+    {
+        forEach([&canvas](const std::shared_ptr<Widget>& widget){
+            widget->onDraw(canvas);
+        });
+    }
+    
+    bool Box::onClickEvent(MouseButton btn, const Position& pos)
+    {
+        auto it = rfind(pos);
+        if (it != rend())
+        {
+            (*it)->handleClickEvent(btn, pos);
+            m_lastClickedWidget.reset();
+            m_lastClickedWidget = (*it);
+            return true;
+        }
+        return false;
+    }
+    
+    bool Box::onReleaseEvent(MouseButton btn, const Position& pos)
+    {
+        if (m_lastClickedWidget != nullptr)
+        {
+            m_lastClickedWidget->handleReleaseEvent(btn, pos);
+            return true;
+        }
+        return false;
+    }
+    
+    bool Box::onKeyEvent(EventType type, int32 keycode)
+    {
+        if (m_lastClickedWidget != nullptr)
+        {
+            m_lastClickedWidget->handleKeyEvent(type, keycode);
+            return true;
+        }
+        return false;
+    }
+    
+    bool Box::onHoverEvent(const Position& pos)
+    {
+        auto it = rfind(pos);
+        if (it != rend())
+        {
+            // Send Leave Event.
+            if (m_currentHoverWidget != (*it) || m_currentHoverWidget == nullptr)
+            {
+                if (m_currentHoverWidget != nullptr)
+                    m_currentHoverWidget->handleLeaveEvent();
+                
+                // New current hovered Widget.
+                m_currentHoverWidget.reset();
+                m_currentHoverWidget = (*it);
+                
+                // Fire Enter event on this and on to child.
+                handleEnterEvent();
+            }
+            
+            m_currentHoverWidget->handleHoverEvent(pos);
+            
+            return true;
+        }else
+        {
+            if (m_currentHoverWidget != nullptr)
+                m_currentHoverWidget->handleLeaveEvent();
+            
+            // New current hovered Widget.
+            m_currentHoverWidget.reset();
+        }
+        
+        return false;
+    }
+    
+    bool Box::onEnterEvent()
+    {
+        if (m_currentHoverWidget != nullptr)
+        {
+            m_currentHoverWidget->handleEnterEvent();
+            return true;
+        }
+        return false;
+    }
+    
+    bool Box::onLeaveEvent()
+    {
+        if (m_currentHoverWidget != nullptr)
+        {
+            m_currentHoverWidget->handleLeaveEvent();
+            
+            // Reset current hovered Widget.
+            m_currentHoverWidget.reset();
+            
+            return true;
+        }
+        return false;
+    }
+    
+    bool Box::onScrollEvent(const Vec2f& offset)
+    {
+        if (m_currentHoverWidget != nullptr)
+        {
+            m_currentHoverWidget->handleScrollEvent(offset);
+            return true;
+        }
+        return false;
+    }
+    
+    void Box::setBackground(const Color& color)
+    {
+        m_background = color;
+    }
+    
+    const Color& Box::getBackground() const
+    {
+        return m_background;
+    }
+}
+
