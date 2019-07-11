71cff4b92a7c38dc6ecc415ebd2a70fb0f550305
Robin Gustafsson <robin.gustafsson14@gmail.com>
2019-07-11 16:31:12 +0200

    Implemented Cell class

---
+++ include/ptk/widgets/Cell.hpp
@@ -0,0 +1,99 @@
+//
+//  core/Cell.hpp
+//  pTK
+//
+//  Created by Robin Gustafsson on 2019-07-11.
+//
+
+#ifndef PTK_CORE_CELL_HPP
+#define PTK_CORE_CELL_HPP
+
+// Local Headers
+#include "ptk/core/Widget.hpp"
+
+// C++ Headers
+#include <memory>
+
+namespace pTK
+{
+    class Cell : public Widget
+    {
+    public:
+        Cell();
+        Cell(const std::shared_ptr<Widget>& widget);
+        ~Cell() = default;
+        
+        void setWidget(const std::shared_ptr<Widget>& widget);
+        std::shared_ptr<Widget> getWidget() const;
+        
+        bool drawChild(Widget* widget) override;
+        
+        void setPosHint(const Position& pos) override;
+        
+        // Event handling
+        
+        /** Function for handling when a key is pressed or released.
+         
+         Derived from EventFunctions.
+         
+         @param type    Key event (press or release)
+         @param int     Keycode
+         */
+        bool onKeyEvent(EventType type, int32 keycode) override;
+        
+        /** Function for handling when mouse is hovering.
+         
+         Derived from EventFunctions.
+         
+         @param pos     position
+         */
+        bool onHoverEvent(const Position& pos) override;
+        
+        /** Function for handling when mouse is entering.
+         
+         Derived from EventFunctions.
+         */
+        bool onEnterEvent() override;
+        
+        /** Function for handling when mouse is leaving.
+         
+         Derived from EventFunctions.
+         */
+        bool onLeaveEvent() override;
+        
+        /** Function for handling when mouse is scrolling.
+         
+         Derived from EventFunctions.
+         
+         @param offset     x and y offset
+         */
+        bool onScrollEvent(const Vec2f& offset) override;
+        
+        /** Function for handling when mouse is clicking.
+         
+         Derived from EventFunctions.
+         
+         @param button      which button on mouse triggered the event.
+         @param position    x and y position
+         */
+        bool onClickEvent(MouseButton btn, const Position& pos) override;
+        
+        /** Function for handling when mouse is released.
+         
+         Derived from EventFunctions.
+         
+         @param button      which button on mouse triggered the event.
+         @param position    x and y position
+         */
+        bool onReleaseEvent(MouseButton btn, const Position& pos) override;
+    private:
+        std::shared_ptr<Widget> m_widget;
+        Size m_widgetSize;
+        Position m_widgetPos;
+        bool m_clicked;
+        bool m_hover;
+    };
+}
+
+
+#endif // PTK_CORE_CELL_HPP
---
+++ src/widgets/Cell.cpp
@@ -0,0 +1,171 @@
+//
+//  core/Cell.cpp
+//  pTK
+//
+//  Created by Robin Gustafsson on 2019-07-11.
+//
+
+// Local Headers
+#include "ptk/widgets/Cell.hpp"
+#include "ptk/Log.hpp"
+
+namespace pTK
+{
+    Cell::Cell()
+    : m_widget{nullptr}, m_widgetSize{}, m_widgetPos{}, m_clicked{false}, m_hover{false}
+    {
+    }
+    
+    Cell::Cell(const std::shared_ptr<Widget>& widget)
+        : m_widget{widget}, m_widgetSize{}, m_widgetPos{}, m_clicked{false}, m_hover{false}
+    {
+        PTK_ASSERT(widget, "Widget is nullptr");
+        m_widgetSize = widget->getSize();
+        m_widgetPos = widget->getPosition();
+        widget->setParent(this);
+    }
+    
+    void Cell::setWidget(const std::shared_ptr<Widget>& widget)
+    {
+        PTK_ASSERT(widget, "Widget is nullptr");
+        m_widget->setParent(nullptr);
+        m_widget = widget;
+        m_widgetSize = widget->getSize();
+        m_widgetPos = widget->getPosition();
+        widget->setParent(this);
+    }
+    
+    std::shared_ptr<Widget> Cell::getWidget() const
+    {
+        return m_widget;
+    }
+    
+    bool Cell::drawChild(Widget* widget)
+    {
+        // Position
+        if (widget->getPosition() != m_widgetPos)
+            widget->setPosHint(m_widgetPos);
+        
+        // Size
+        Size wSize = widget->getSize();
+        if (wSize != m_widgetSize)
+        {
+            m_widgetSize = wSize;
+            Size cellSize = getSize();
+            if ((wSize.width > cellSize.width) || (wSize.height > cellSize.height))
+                setSize(wSize);
+            
+            return true;
+        }else
+        {
+            draw();
+            return true;
+        }
+        
+        return false;
+    }
+    
+    void Cell::setPosHint(const Position& pos)
+    {
+        Position currentPos = getPosition();
+        Position deltaPos = pos - currentPos;
+        
+        Position wPos = m_widget->getPosition();
+        wPos += deltaPos;
+        m_widgetPos = wPos;
+        m_widget->setPosHint(wPos);
+        
+        Widget::setPosHint(pos);
+    }
+    
+    bool Cell::onClickEvent(MouseButton btn, const Position& pos)
+    {
+        Position wPos = m_widget->getPosition();
+        Size wSize = m_widget->getSize();
+        if ((wPos.x <= pos.x) && (wPos.x + wSize.width >= pos.x))
+        {
+            if ((wPos.y <= pos.y) && (wPos.y + wSize.height >= pos.y))
+            {
+                m_widget->handleClickEvent(btn, pos);
+                m_clicked = true;
+                return true;
+            }
+        }
+        m_clicked = false;
+        return false;
+    }
+    
+    bool Cell::onReleaseEvent(MouseButton btn, const Position& pos)
+    {
+        if (m_clicked)
+        {
+            m_widget->handleReleaseEvent(btn, pos);
+            return true;
+        }
+        return false;
+    }
+    
+    bool Cell::onKeyEvent(EventType type, int32 keycode)
+    {
+        if (m_clicked)
+        {
+            m_widget->handleKeyEvent(type, keycode);
+            return true;
+        }
+        return false;
+    }
+    
+    bool Cell::onHoverEvent(const Position& pos)
+    {
+        Position wPos = m_widget->getPosition();
+        Size wSize = m_widget->getSize();
+        if ((wPos.x <= pos.x) && (wPos.x + wSize.width >= pos.x))
+        {
+            if ((wPos.y <= pos.y) && (wPos.y + wSize.height >= pos.y))
+            {
+                m_widget->handleHoverEvent(pos);
+                m_hover = true;
+                return true;
+            }
+        }else if (m_hover)
+        {
+            m_widget->handleLeaveEvent();
+            m_hover = false;
+            return true;
+        }
+        
+        m_hover = false;
+        return false;
+    }
+    
+    bool Cell::onEnterEvent()
+    {
+        if (m_hover)
+        {
+            m_widget->handleEnterEvent();
+            return true;
+        }
+        return false;
+    }
+    
+    bool Cell::onLeaveEvent()
+    {
+        if (m_hover)
+        {
+            m_widget->handleLeaveEvent();
+            m_hover = false;
+            return true;
+        }
+        return false;
+    }
+    
+    bool Cell::onScrollEvent(const Vec2f& offset)
+    {
+        if (m_hover)
+        {
+            m_widget->handleScrollEvent(offset);
+            return true;
+        }
+        return false;
+    }
+}
