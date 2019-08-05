8bc880f631f399bd8bf60f56048d11195e28ac0a
Robin Gustafsson <robin.gustafsson14@gmail.com>
2019-08-05 02:41:45 +0200

    Merged Box and Container

---
+++ include/ptk/core/Container.hpp
@@ -0,0 +1,336 @@
+//
+//  core/Container.hpp
+//  pTK
+//
+//  Created by Robin Gustafsson on 2019-06-20.
+//
+
+#ifndef PTK_CORE_CONTAINER_HPP
+#define PTK_CORE_CONTAINER_HPP
+
+// Local Headers
+#include "ptk/core/Types.hpp"
+#include "ptk/core/Widget.hpp"
+#include "ptk/util/Color.hpp"
+
+// C++ Headers
+#include <vector>
+#include <functional>
+#include <algorithm>
+
+namespace pTK
+{
+    /** Container class implementation.
+     
+     Base class for handling a collection of Widgets.
+     */
+    class Container : public Widget
+    {
+    public:
+        /** typedefs for Container.
+         */
+        using type                      = std::shared_ptr<Widget>;
+        using container_type            = std::vector<type>;
+        using iterator                  = container_type::iterator;
+        using reverse_iterator          = container_type::reverse_iterator;
+        using const_iterator            = container_type::const_iterator;
+        using const_reverse_iterator    = container_type::const_reverse_iterator;
+        using size_type                 = uint;
+        
+        /** Constructs Container with default values.
+         
+         @return    default initialized Container
+         */
+        Container();
+        
+        /** Constructs Container with default values with item.
+         
+         @return    default initialized Container
+         */
+        Container(const type& item);
+        
+        /** Constructs Container with default values with list.
+         
+         @return    default initialized Container
+         */
+        Container(std::initializer_list<type> list);
+        
+        /** Constructs Container with default values with Container.
+         
+         @return    default initialized Container
+         */
+        Container(const Container& rhs);
+        
+        virtual ~Container();
+        
+        /** Container assignment operator.
+         
+         @param rhs     Container to copy from
+         @return        Container with values from rhs
+         */
+        Container& operator=(const Container& rhs);
+        
+        /** Funtion for adding a widget to the Container.
+         
+         @param widget  widget to add
+         */
+        virtual bool add(const std::shared_ptr<Widget>& widget);
+        
+        /** Funtion for removing a widget in the Container.
+         
+         @param widget  widget to remove
+         */
+        virtual void remove(const std::shared_ptr<Widget>& widget);
+        
+        /** Function for retrieving the element at the index in the Container.
+         
+         @param index   Index
+         @return        element in Container
+         */
+        type at(unsigned int index);
+        
+        /** Operator for retrieving the element at the index in the Container.
+         
+         @param index   Index
+         @return        element in Container
+         */
+        type operator[](unsigned int index) const;
+        
+        /** Function for retrieving the size of the Container.
+         
+         */
+        size_type size() const;
+        
+        /** Function for checking if the Container is empty.
+         
+         @return    empty
+         */
+        bool empty() const;
+        
+        /** Function for retrieving the first element in the container.
+         
+         Should in no circumstances be called when the container is empty!
+         This is considered to be undefined behavior!
+         
+         @return    first element in Container
+         */
+        type front() const;
+        
+        /** Function for retrieving the last element in the container.
+         
+         Should in no circumstances be called when the container is empty!
+         This is considered to be undefined behavior!
+         
+         @return    last element in container
+         */
+        type back() const;
+        
+        /** Function for retrieving the an iterator that points to the first
+         value in the Container.
+         
+         The iterator may be equal to end iterator if the Container is empty.
+         
+         @return    iterator
+         */
+        iterator begin();
+        
+        /** Function for retrieving the special iterator referring to
+         the past-the-end of the Container.
+         
+         The iterator should never be dereferenced, due to the fact that the iterator
+         does not point to a value and should therefore only be used for checking.
+         
+         @return    iterator
+         */
+        iterator end();
+        
+        /** Function for retrieving the an const iterator that points to the first
+         value in the Container.
+         
+         The iterator may be equal to end iterator if the Container is empty.
+         
+         @return    const iterator
+         */
+        const_iterator cbegin() const;
+        
+        /** Function for retrieving the special const iterator referring to
+         the past-the-end of the Container.
+         
+         The iterator should never be dereferenced, due to the fact that the iterator
+         does not point to a value and should therefore only be used for checking.
+         
+         @return    const iterator
+         */
+        const_iterator cend() const;
+        
+        /** Function for retrieving the an iterator that points to the last
+         value in the Container.
+         
+         This iterator is working in reverse. Meaning that is starts at the end
+         and is moving to the beginning.
+         
+         The iterator may be equal to rend iterator if the Container is empty.
+         
+         @return    const iterator
+         */
+        reverse_iterator rbegin();
+        
+        /** Function for retrieving the special const iterator referring to
+         the past-the-end of the Container.
+         
+         The iterator should never be dereferenced, due to the fact that the iterator
+         does not point to a value and should therefore only be used for checking.
+         
+         @return    const iterator
+         */
+        reverse_iterator rend();
+        
+        /** Funtion for finding a widget in the Container.
+         Function will look for a widget with the specified name and it
+         will grab the first with the matching name.
+         
+         Meaning that if two Widgets or more have the same name, only the
+         first added will be found.
+         
+         @param name    name to find
+         @return        const_iterator to widget or end const_iterator
+         */
+        const_iterator find(const std::string& name);
+        
+        /** Funtion for finding a widget in the Container.
+         Function can be used to check if a pointer to Widget exists.
+         
+         @param widget  widget to find
+         @return        const_iterator to widget or end const_iterator
+         */
+        const_iterator find(const Widget* widget);
+        
+        /** Funtion for finding a widget in the Container.
+         
+         @param pos     pos to find
+         @return        const_iterator to widget or end const_iterator
+         */
+        const_iterator find(const Position& pos);
+        
+        /** Funtion for finding a widget in the Container.
+         In reverse order.
+         
+         @param pos     pos to find
+         @return        const_iterator to widget or end const_iterator
+         */
+        reverse_iterator rfind(const Position& pos);
+        
+        /** Function for looping over the widgets in the container.
+         
+         @param func    function to call for each widget.
+         */
+        void forEach(const std::function<void(const type& item)>& func) const;
+        
+        /** Draw function.
+         Function is called when it is time to draw.
+         
+         Derived from Drawable.
+         @param canvas  canvas to draw to
+         */
+        void onDraw(SkCanvas* canvas) override;
+        
+        // Event handling
+        
+        /** Function for handling when a key is pressed or released.
+         
+         Derived from EventFunctions.
+         
+         @param type    Key event (press or release)
+         @param int     Keycode
+         */
+        bool onKeyEvent(EventType type, int32 keycode) override;
+        
+        /** Function for handling when mouse is hovering.
+         
+         Derived from EventFunctions.
+         
+         @param pos     position
+         */
+        bool onHoverEvent(const Position& pos) override;
+        
+        /** Function for handling when mouse is entering.
+         
+         Derived from EventFunctions.
+         */
+        bool onEnterEvent() override;
+        
+        /** Function for handling when mouse is leaving.
+         
+         Derived from EventFunctions.
+         */
+        bool onLeaveEvent() override;
+        
+        /** Function for handling when mouse is scrolling.
+         
+         Derived from EventFunctions.
+         
+         @param offset     x and y offset
+         */
+        bool onScrollEvent(const Vec2f& offset) override;
+        
+        /** Function for handling when mouse is clicking.
+         
+         Derived from EventFunctions.
+         
+         @param button      which button on mouse triggered the event.
+         @param position    x and y position
+         */
+        bool onClickEvent(MouseButton btn, const Position& pos) override;
+        
+        /** Function for handling when mouse is released.
+         
+         Derived from EventFunctions.
+         
+         @param button      which button on mouse triggered the event.
+         @param position    x and y position
+         */
+        bool onReleaseEvent(MouseButton btn, const Position& pos) override;
+        
+        /** Funtion for setting the background of the Container.
+         
+         @param color   background color
+         */
+        void setBackground(const Color& color);
+        
+        /** Funtion for getting the background of the Container.
+         
+         @return    current background color
+         */
+        const Color& getBackground() const;
+        
+    protected:
+        
+        /** Funtion for getting the minimal size of all childs in Container.
+         
+         @return    minimal size of all childs
+         */
+        virtual Size calculateMinSize() const;
+        
+        /** Funtion for getting the miximal size of all childs in Container.
+         
+         @return    miximal size of all childs
+         */
+        virtual Size calculateMaxSize() const;
+        
+        /** Funtion for drawing all the childs in the Container.
+         
+         */
+        void drawChilds(SkCanvas* canvas);
+        
+    private:
+        container_type m_holder;
+        
+        // Variables
+        Color m_background;
+        Widget* m_lastClickedWidget;
+        Widget* m_currentHoverWidget;
+    };
+}
+
+#endif // PTK_CORE_CONTAINER_HPP
+
---
+++ src/core/Container.cpp
@@ -0,0 +1,367 @@
+//
+//  core/Box.hpp
+//  pTK
+//
+//  Created by Robin Gustafsson on 2019-07-03.
+//
+
+// Local Headers
+#include "ptk/core/Container.hpp"
+
+namespace pTK
+{
+    Container::Container()
+        : Widget(),
+            m_holder{}, m_background{0xf5f5f5ff}, m_lastClickedWidget{nullptr},
+            m_currentHoverWidget{nullptr}
+    {
+    }
+    
+    Container::Container(const type& item)
+        : Widget(),
+            m_holder{}, m_background{0xf5f5f5ff}, m_lastClickedWidget{nullptr},
+            m_currentHoverWidget{nullptr}
+    {
+        add(item);
+    }
+    
+    Container::Container(std::initializer_list<type> list)
+        : Widget(),
+            m_holder{}, m_background{0xf5f5f5ff}, m_lastClickedWidget{nullptr},
+            m_currentHoverWidget{nullptr}
+    {
+        m_holder.reserve(list.size());
+        for (const type& item : list)
+            add(item);
+    }
+    
+    Container::Container(const Container& rhs)
+    {
+        m_holder.reserve(rhs.size());
+        for (auto it{rhs.cbegin()}; it != rhs.cend(); it++)
+            add((*it));
+    }
+    
+    Container::~Container()
+    {
+        forEach([](const Container::type& item){
+            item->setParent(nullptr);
+        });
+    }
+    
+    Container& Container::operator=(const Container& rhs)
+    {
+        if (this == &rhs)
+            return *this;
+        
+        m_holder.reserve(rhs.size());
+        for (auto it{rhs.cbegin()}; it != rhs.cend(); it++)
+            add((*it));
+        
+        return *this;
+    }
+    
+    bool Container::add(const std::shared_ptr<Widget>& widget)
+    {
+        if (find(widget.get()) == cend())
+        {
+            widget->setParent(this);
+            m_holder.push_back(widget);
+            return true;
+        }
+        return false;
+    }
+    
+    void Container::remove(const std::shared_ptr<Widget>& widget)
+    {
+        const_iterator it = find(widget.get());
+        if (it != cend())
+        {
+            widget->setParent(nullptr);
+            m_holder.erase(it);
+        }
+    }
+    
+    Container::type Container::at(unsigned int index)
+    {
+        if (index < 0 || index >= size())
+            throw std::out_of_range("Index is out of range!");
+        
+        return m_holder.at(index);
+    }
+    
+    Container::type Container::operator[](unsigned int index) const
+    {
+        if (index < 0 || index >= size())
+            throw std::out_of_range("Index is out of range!");
+        
+        return m_holder[index];
+    }
+    
+    Container::size_type Container::size() const
+    {
+        return m_holder.size();
+    }
+    
+    bool Container::empty() const
+    {
+        return m_holder.empty();
+    }
+    
+    Container::type Container::front() const
+    {
+        return m_holder.front();
+    }
+    
+    Container::type Container::back() const
+    {
+        return m_holder.back();
+    }
+    
+    Container::iterator Container::begin()
+    {
+        return m_holder.begin();
+    }
+    
+    Container::iterator Container::end()
+    {
+        return m_holder.end();
+        
+    }
+    
+    Container::const_iterator Container::cbegin() const
+    {
+        return m_holder.cbegin();
+    }
+    
+    Container::const_iterator Container::cend() const
+    {
+        return m_holder.cend();
+        
+    }
+    
+    Container::reverse_iterator Container::rbegin()
+    {
+        return m_holder.rbegin();
+    }
+    
+    Container::reverse_iterator Container::rend()
+    {
+        return m_holder.rend();
+    }
+    
+    Container::const_iterator Container::find(const std::string& name)
+    {
+        for (auto it{cbegin()}; it != cend(); ++it)
+            if ((*it)->getName() == name)
+                return it;
+        
+        return cend();
+    }
+    
+    Container::const_iterator Container::find(const Widget* widget)
+    {
+        for (auto it{cbegin()}; it != cend(); ++it)
+            if ((*it).get() == widget)
+                return it;
+        
+        return cend();
+    }
+    
+    Container::const_iterator Container::find(const Position& pos)
+    {
+        for (auto it{cbegin()}; it != cend(); ++it)
+        {
+            Position wPos = (*it)->getPosition();
+            Size wSize = (*it)->getSize();
+            
+            if ((wPos.x <= pos.x) && (wPos.x + wSize.width >= pos.x))
+                if ((wPos.y <= pos.y) && (wPos.y + wSize.height >= pos.y))
+                    return it;
+        }
+        
+        return cend();
+    }
+    
+    Container::reverse_iterator Container::rfind(const Position& pos)
+    {
+        for (auto it{rbegin()}; it != rend(); ++it)
+        {
+            Position wPos = (*it)->getPosition();
+            Size wSize = (*it)->getSize();
+            
+            if ((wPos.x <= pos.x) && (wPos.x + wSize.width >= pos.x))
+                if ((wPos.y <= pos.y) && (wPos.y + wSize.height >= pos.y))
+                    return it;
+        }
+        
+        return rend();
+    }
+    
+    void Container::forEach(const std::function<void(const Container::type& item)>& func) const
+    {
+        for (const_iterator it = cbegin(); it != cend(); it++)
+            func((*it));
+    }
+    
+    void Container::onDraw(SkCanvas* canvas)
+    {
+        // Set Size and Position
+        SkPoint pos{convertToSkPoint(getPosition())};
+        SkPoint size{convertToSkPoint(getSize())};
+        size += pos; // skia needs the size to be pos+size.
+        
+        // Set Color
+        SkPaint paint;
+        paint.setAntiAlias(true);
+        paint.setARGB(m_background.a, m_background.r, m_background.g, m_background.b);
+        
+        // Draw Rect
+        SkRect rect;
+        rect.set(pos, size);
+        paint.setStyle(SkPaint::kStrokeAndFill_Style);
+        canvas->drawRoundRect(rect, 0, 0, paint);
+        
+        drawChilds(canvas);
+    }
+    
+    void Container::drawChilds(SkCanvas* canvas)
+    {
+        for (iterator it = begin(); it != end(); it++)
+            (*it)->onDraw(canvas);
+    }
+    
+    bool Container::onClickEvent(MouseButton btn, const Position& pos)
+    {
+        for (auto it = begin(); it != end(); it++)
+        {
+            Position wPos = (*it)->getPosition();
+            Size wSize = (*it)->getSize();
+            if ((wPos.x <= pos.x) && (wPos.x + wSize.width >= pos.x))
+            {
+                if ((wPos.y <= pos.y) && (wPos.y + wSize.height >= pos.y))
+                {
+                    (*it)->handleClickEvent(btn, pos);
+                    m_lastClickedWidget = (*it).get();
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+    }
+    
+    bool Container::onReleaseEvent(MouseButton btn, const Position& pos)
+    {
+        if (m_lastClickedWidget != nullptr)
+        {
+            m_lastClickedWidget->handleReleaseEvent(btn, pos);
+            return true;
+        }
+        return false;
+    }
+    
+    bool Container::onKeyEvent(EventType type, int32 keycode)
+    {
+        if (m_lastClickedWidget != nullptr)
+        {
+            m_lastClickedWidget->handleKeyEvent(type, keycode);
+            return true;
+        }
+        return false;
+    }
+    
+    bool Container::onHoverEvent(const Position& pos)
+    {
+        for (auto it = begin(); it != end(); it++)
+        {
+            Position wPos = (*it)->getPosition();
+            Size wSize = (*it)->getSize();
+            if ((wPos.x <= pos.x) && (wPos.x + wSize.width >= pos.x))
+            {
+                if ((wPos.y <= pos.y) && (wPos.y + wSize.height >= pos.y))
+                {
+                    // Send Leave Event.
+                    if (m_currentHoverWidget != (*it).get() || m_currentHoverWidget == nullptr)
+                    {
+                        if (m_currentHoverWidget != nullptr)
+                            m_currentHoverWidget->handleLeaveEvent();
+                        
+                        // New current hovered Widget.
+                        m_currentHoverWidget = (*it).get();
+                        
+                        // Fire Enter event on this and on to child.
+                        handleEnterEvent();
+                    }
+                    
+                    m_currentHoverWidget->handleHoverEvent(pos);
+                    
+                    return true;
+                }
+            }
+        }
+        
+        if (m_currentHoverWidget != nullptr)
+            m_currentHoverWidget->handleLeaveEvent();
+        
+        // New current hovered Widget.
+        m_currentHoverWidget = nullptr;
+        
+        return false;
+    }
+    
+    bool Container::onEnterEvent()
+    {
+        if (m_currentHoverWidget != nullptr)
+        {
+            m_currentHoverWidget->handleEnterEvent();
+            return true;
+        }
+        return false;
+    }
+    
+    bool Container::onLeaveEvent()
+    {
+        if (m_currentHoverWidget != nullptr)
+        {
+            m_currentHoverWidget->handleLeaveEvent();
+            
+            // Reset current hovered Widget.
+            m_currentHoverWidget = nullptr;
+            
+            return true;
+        }
+        return false;
+    }
+    
+    bool Container::onScrollEvent(const Vec2f& offset)
+    {
+        if (m_currentHoverWidget != nullptr)
+        {
+            m_currentHoverWidget->handleScrollEvent(offset);
+            return true;
+        }
+        return false;
+    }
+    
+    void Container::setBackground(const Color& color)
+    {
+        m_background = color;
+    }
+    
+    const Color& Container::getBackground() const
+    {
+        return m_background;
+    }
+    
+    Size Container::calculateMinSize() const
+    {
+        return Size(-1.0f, -1.0f);
+    }
+    
+    Size Container::calculateMaxSize() const
+    {
+        return Size(-1.0f, -1.0f);
+    }
+}
+
