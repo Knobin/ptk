df777ea936f280c20df880fd86e7cc7e24ee3c0c
Robin Gustafsson <robin.gustafsson14@gmail.com>
2019-09-11 20:53:55 +0200

    Implemented Singleton class

---
+++ include/ptk/util/Singleton.hpp
@@ -0,0 +1,29 @@
+//
+//  util/Singleton.hpp
+//  pTK
+//
+//  Created by Robin Gustafsson on 2019-06-29.
+//
+
+#ifndef PTK_UTIL_SINGLETON_HPP
+#define PTK_UTIL_SINGLETON_HPP
+
+// Local Headers
+#include "ptk/util/NonCopyable.hpp"
+#include "ptk/util/NonMovable.hpp"
+
+namespace pTK
+{
+    /** Singeton class implementation.
+     
+     Class for forbidding copying and moving.
+     */
+    class Singleton : public NonMovable, public NonCopyable
+    {
+    public:
+        virtual ~Singleton() = default;
+    };
+}
+
+#endif // PTK_UTIL_SINGLETON_HPP
+
