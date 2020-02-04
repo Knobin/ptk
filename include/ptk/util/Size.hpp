42af95efbe1bcc24b32176e44b8051d0dbc69f8b
Robin Gustafsson <robin.gustafsson14@gmail.com>
2020-02-04 14:52:59 +0100

    Added missing comparison operators for Size

--- include/ptk/util/Size.hpp
+++ include/ptk/util/Size.hpp
@@ -17,7 +17,7 @@
 namespace pTK
 {
     /** Size class implementation.
-         
+
         This class is low level class handling size (width and height).
     */
     class Size
@@ -30,41 +30,45 @@ namespace pTK
             static const value_type Min;
             static const value_type Max;
         };
-        
+
         static const Size Max;
         static const Size Min;
-        
+
     public:
         /** Constructs Size with default values.
-         
+
             @return    default initialized Size
         */
         Size();
 
         /** Constructs Size with default values width and height.
-            
+
             @param width   width
             @param height  height
             @return        default initialized Size
         */
         Size(value_type other_width, value_type other_height);
         ~Size() = default;
-        
+
         /** Function for setting size.
-         
+
             @param width   new width
             @param height  new height
         */
         void setSize(value_type other_width, value_type other_height);
-        
+
         value_type width;
         value_type height;
     };
-    
+
     // Comparison operators.
     bool operator==(const Size& lhs, const Size& rhs);
     bool operator!=(const Size& lhs, const Size& rhs);
-    
+    bool operator<(const Size& lhs, const Size& rhs);
+    bool operator>(const Size& lhs, const Size& rhs);
+    bool operator<=(const Size& lhs, const Size& rhs);
+    bool operator>=(const Size& lhs, const Size& rhs);
+
     // Binary arithmetic operators.
     Size operator+(const Size& lhs, const Size& rhs);
     Size operator-(const Size& lhs, const Size& rhs);
--- src/util/Size.cpp
+++ src/util/Size.cpp
@@ -12,91 +12,113 @@ namespace pTK
 {
     const Size::value_type Size::Limits::Min = 0;
     const Size::value_type Size::Limits::Max = std::numeric_limits<Size::value_type>::max();
-    
+
     const Size Size::Min(Size::Limits::Min, Size::Limits::Min);
     const Size Size::Max(Size::Limits::Max, Size::Limits::Max);
-    
+
     Size::Size()
         : width{Limits::Min}, height{Limits::Min}
     {
-        
+
     }
-    
+
     Size::Size(value_type other_width, value_type other_height)
         : width{other_width}, height{other_height}
     {
-        
+
     }
-    
+
     void Size::setSize(value_type other_width, value_type other_height)
     {
         width = other_width;
         height = other_height;
     }
-    
+
     // Comparison operators.
     bool operator==(const Size& lhs, const Size& rhs)
     {
-        return ((lhs.width == rhs.width) && (lhs.height == rhs.height));
+        return ((lhs.width == rhs.width)
+                && (lhs.height == rhs.height));
     }
-    
+
     bool operator!=(const Size& lhs, const Size& rhs)
     {
         return !(lhs == rhs);
     }
-    
+
+    bool operator<(const Size& lhs, const Size& rhs)
+    {
+        return rhs > lhs;
+    }
+
+    bool operator>(const Size& lhs, const Size& rhs)
+    {
+        return ((lhs.width > rhs.width)
+                && (lhs.height > rhs.height));
+    }
+
+    bool operator<=(const Size& lhs, const Size& rhs)
+    {
+        return !(lhs > rhs);
+    }
+
+    bool operator>=(const Size& lhs, const Size& rhs)
+    {
+        return !(lhs < rhs);
+    }
+
     // Binary arithmetic operators.
     Size operator+(const Size& lhs, const Size& rhs)
     {
         Size newSize;
         newSize.width = lhs.width + rhs.width;
         newSize.height = lhs.height + rhs.height;
-        
+
         return newSize;
     }
-    
+
     Size operator-(const Size& lhs, const Size& rhs)
     {
         Size newSize;
         newSize.width = lhs.width - rhs.width;
         newSize.height = lhs.height - rhs.height;
-        
+
         return newSize;
     }
-    
+
     Size operator*(const Size& lhs, const Size& rhs)
     {
         Size newSize;
         newSize.width = lhs.width * rhs.width;
         newSize.height = lhs.height * rhs.height;
-        
+
         return newSize;
     }
-    
+
     Size operator/(const Size& lhs, const Size& rhs)
     {
         Size newSize;
         newSize.width = lhs.width / rhs.width;
         newSize.height = lhs.height / rhs.height;
-        
+
         return newSize;
     }
-    
+
     Size& operator+=(Size& lhs, const Size& rhs)
     {
         return lhs = lhs + rhs;
     }
-    
+
     Size& operator-=(Size& lhs, const Size& rhs)
     {
         return lhs = lhs - rhs;
     }
-    
+
     Size& operator*=(Size& lhs, const Size& rhs)
     {
         return lhs = lhs * rhs;
     }
-    
+
     Size& operator/=(Size& lhs, const Size& rhs)
     {
         return lhs = lhs / rhs;
