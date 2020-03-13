43b310ed54656612152e208786bb8040e47fae38
Robin Gustafsson <robin.gustafsson14@gmail.com>
2020-03-13 18:55:34 +0100

    Fixed wrong file added in src/CMakeLists.txt and removed compiler warnings

--- include/ptk/platform/Backend.hpp
+++ include/ptk/platform/Backend.hpp
@@ -12,11 +12,9 @@
 #include "ptk/platform/win32/Win32Backend.hpp"
 using BACKEND = pTK::Win32Backend;
 #elif defined(PTK_PLATFORM_UNIX)
-#pragma message("X11 or Wayland is not implemented, using GLFW with OpenGL")
 #include "ptk/platform/glfw/GLFWBackend.hpp"
 using BACKEND = pTK::GLFWBackend;
 #elif defined(PTK_PLATFORM_APPLE)
-#pragma message("Cocoa is not implemented, using GLFW with OpenGL")
 #include "ptk/platform/glfw/GLFWBackend.hpp"
 using BACKEND = pTK::GLFWBackend;
 #else
