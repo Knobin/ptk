7c7e14358ea7f784d665ddd7ac2dbbd03487d07e
Robin Gustafsson <robin.gustafsson14@gmail.com>
2020-03-16 06:37:56 +0100

    Added custom exceptions

---
+++ include/ptk/core/Exception.hpp
@@ -0,0 +1,30 @@
+//
+//  core/Exception.hpp
+//  pTK
+//
+//  Created by Robin Gustafsson on 2020-02-09.
+//
+
+// C++ Headers
+#include <stdexcept>
+
+namespace pTK
+{
+    class Exception : public std::runtime_error
+    {
+    public:
+        using runtime_error::runtime_error;
+    };
+
+    class WindowError : public Exception
+    {
+    public:
+        using Exception::Exception;
+    };
+
+    class ContextError : public Exception
+    {
+    public:
+        using Exception::Exception;
+    };
+}
