e595f00f9dd38572ea66befc3d61f7ed6ec557b0
Robin Gustafsson <robin.gustafsson14@gmail.com>
2020-04-19 02:17:33 +0200

    Added basic keycodes

---
+++ include/ptk/events/KeyCodes.hpp
@@ -0,0 +1,58 @@
+//
+//  events/KeyCodes.hpp
+//  pTK
+//
+//  Created by Robin Gustafsson on 2020-04-19.
+//
+
+#ifndef PTK_KEYCODES_HPP
+#define PTK_KEYCODES_HPP
+
+// TODO: Add more keycodes.
+
+#define PTK_KEY_UNKNOWN         -1
+
+#define PTK_KEY_ESCAPE          27
+#define PTK_KEY_SPACE           32
+
+// Numbers
+#define PTK_KEY_0               48
+#define PTK_KEY_1               49
+#define PTK_KEY_2               50
+#define PTK_KEY_3               51
+#define PTK_KEY_4               52
+#define PTK_KEY_5               53
+#define PTK_KEY_6               54
+#define PTK_KEY_7               55
+#define PTK_KEY_8               56
+#define PTK_KEY_9               57
+
+// Letters
+#define PTK_KEY_A               65
+#define PTK_KEY_B               66
+#define PTK_KEY_C               67
+#define PTK_KEY_D               68
+#define PTK_KEY_E               69
+#define PTK_KEY_F               70
+#define PTK_KEY_G               71
+#define PTK_KEY_H               72
+#define PTK_KEY_I               73
+#define PTK_KEY_J               74
+#define PTK_KEY_K               75
+#define PTK_KEY_L               76
+#define PTK_KEY_M               77
+#define PTK_KEY_N               78
+#define PTK_KEY_O               79
+#define PTK_KEY_P               80
+#define PTK_KEY_Q               81
+#define PTK_KEY_R               82
+#define PTK_KEY_S               83
+#define PTK_KEY_T               84
+#define PTK_KEY_U               85
+#define PTK_KEY_V               86
+#define PTK_KEY_W               87
+#define PTK_KEY_X               88
+#define PTK_KEY_Y               89
+#define PTK_KEY_Z               90
+
+#endif //PTK_KEYCODES_HPP
