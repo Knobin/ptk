8c7ddddc8ad1470c2dc7b663da1587a0a6e03435
Robin Gustafsson <robin.gustafsson14@gmail.com>
2020-10-08 01:01:30 +0200

    Fixed fatal log

--- include/ptk/Log.hpp
+++ include/ptk/Log.hpp
@@ -36,7 +36,7 @@
     #define PTK_ERROR(...)  pTK::Log::getLogger()->error(__VA_ARGS__)
     #define PTK_TRACE(...)  pTK::Log::getLogger()->trace(__VA_ARGS__)
     #define PTK_INFO(...)   pTK::Log::getLogger()->info(__VA_ARGS__)
-    #define PTK_FATAL(...)  pTK::Log::getLogger()->fatal(__VA_ARGS__)
+    #define PTK_FATAL(...)  pTK::Log::getLogger()->critical(__VA_ARGS__)
 #else
     #define PTK_INIT_LOGGING(...)
     #define PTK_WARN(...)
