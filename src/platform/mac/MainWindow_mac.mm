46c098064f8d4fe5be0a1587659c0b0b72ec531c
Robin Gustafsson <robin.gustafsson14@gmail.com>
2020-10-10 03:55:17 +0200

    Added keycodes to key events to MacOS window

--- src/platform/mac/MainWindow_mac.mm
+++ src/platform/mac/MainWindow_mac.mm
@@ -29,6 +29,37 @@
 
 namespace pTK
 {
+    static std::map<byte, KeyCode> initKeyCodes() noexcept
+    {
+        std::map<byte, KeyCode> map{};
+        map[0x31] = Key::Space; map[0x35] = Key::Escape;
+        map[0x1D] = Key::D0; map[0x12] = Key::D1; map[0x13] = Key::D2; map[0x14] = Key::D3;
+        map[0x15] = Key::D4; map[0x17] = Key::D5; map[0x16] = Key::D6; map[0x1A] = Key::D7;
+        map[0x1C] = Key::D8; map[0x19] = Key::D9;
+        map[0x52] = Key::D0; map[0x53] = Key::D1; map[0x54] = Key::D2; map[0x55] = Key::D3;
+        map[0x56] = Key::D4; map[0x57] = Key::D5; map[0x58] = Key::D6; map[0x59] = Key::D7;
+        map[0x5B] = Key::D8; map[0x5C] = Key::D9;
+        map[0x00] = Key::A; map[0x0B] = Key::B; map[0x08] = Key::C; map[0x02] = Key::D;
+        map[0x0E] = Key::E; map[0x03] = Key::F; map[0x05] = Key::G; map[0x04] = Key::H;
+        map[0x22] = Key::I; map[0x26] = Key::J; map[0x28] = Key::K; map[0x25] = Key::L;
+        map[0x2E] = Key::M; map[0x2D] = Key::N; map[0x1F] = Key::O; map[0x23] = Key::P;
+        map[0x0C] = Key::Q; map[0x0F] = Key::R; map[0x01] = Key::S; map[0x11] = Key::T;
+        map[0x20] = Key::U; map[0x09] = Key::V; map[0x0D] = Key::W; map[0x07] = Key::X;
+        map[0x10] = Key::Y; map[0x06] = Key::Z;
+        return map;
+    }
+
+    static const std::map<byte, KeyCode> s_keyMap{initKeyCodes()};
+
+    static Key translateKeyCode(byte code)
+    {
+        std::map<byte, KeyCode>::const_iterator it{s_keyMap.find(code)};
+        if (it != s_keyMap.cend())
+            return it->second;
+
+        return Key::None;
+    }
+
     struct MainWindow_mac::WinData
     {
         NSWindow *window;
@@ -196,6 +227,23 @@ namespace pTK
     window->parent()->sendEvent(&evt);
 }
 
+
+- (void)keyDown:(NSEvent *)event
+{
+    const int code = event.keyCode;
+    pTK::KeyEvent evt{pTK::Event::Type::KeyPressed, pTK::translateKeyCode(static_cast<byte>(code))};
+    window->parent()->sendEvent(&evt);
+    
+    [self interpretKeyEvents:@[event]];
+}
+
+- (void)keyUp:(NSEvent *)event
+{
+    const int code = event.keyCode;
+    pTK::KeyEvent evt{pTK::Event::Type::KeyReleased, pTK::translateKeyCode(static_cast<byte>(code))};
+    window->parent()->sendEvent(&evt);
+}
+
 - (BOOL)acceptsFirstMouse:(NSEvent*) __unused event
 {
     return YES;
@@ -237,42 +285,6 @@ namespace pTK
 
 namespace pTK
 {
-    /*
-
-    static std::map<byte, KeyCode> initKeyCodes() noexcept
-    {
-        std::map<byte, KeyCode> map{};
-        map[VK_SPACE] = Key::Space; map[VK_ESCAPE] = Key::Escape;
-        map[0x30] = Key::D0; map[0x31] = Key::D1; map[0x32] = Key::D2; map[0x33] = Key::D3;
-        map[0x34] = Key::D4; map[0x35] = Key::D5; map[0x36] = Key::D6; map[0x37] = Key::D7;
-        map[0x38] = Key::D8; map[0x39] = Key::D9;
-        map[VK_NUMPAD0] = Key::D0; map[VK_NUMPAD1] = Key::D1; map[VK_NUMPAD2] = Key::D2;
-        map[VK_NUMPAD3] = Key::D3; map[VK_NUMPAD4] = Key::D4; map[VK_NUMPAD5] = Key::D5;
-        map[VK_NUMPAD6] = Key::D6; map[VK_NUMPAD7] = Key::D7; map[VK_NUMPAD8] = Key::D8;
-        map[VK_NUMPAD9] = Key::D9;
-        map[0x41] = Key::A; map[0x42] = Key::B; map[0x43] = Key::C; map[0x44] = Key::D;
-        map[0x45] = Key::E; map[0x46] = Key::F; map[0x47] = Key::G; map[0x48] = Key::H;
-        map[0x49] = Key::I; map[0x4A] = Key::J; map[0x4B] = Key::K; map[0x4C] = Key::L;
-        map[0x4D] = Key::M; map[0x4E] = Key::N; map[0x4F] = Key::O; map[0x50] = Key::P;
-        map[0x51] = Key::Q; map[0x52] = Key::R; map[0x53] = Key::S; map[0x54] = Key::T;
-        map[0x55] = Key::U; map[0x56] = Key::V; map[0x57] = Key::W; map[0x58] = Key::X;
-        map[0x59] = Key::Y; map[0x5A] = Key::Z;
-        return map;
-    }
-
-    static const std::map<byte, KeyCode> s_keyMap{initKeyCodes()};
-
-    static Key translateKeyCode(byte code)
-    {
-        std::map<byte, KeyCode>::const_iterator it{s_keyMap.find(code)};
-        if (it != s_keyMap.cend())
-            return it->second;
-
-        return Key::None;
-    }
-
-    */
-
     static std::unique_ptr<ContextBase> createMacContext(BackendType type, MainWindow_mac::WinData *data, void *view)
     {
 #ifdef PTK_DEBUG
