5571f85d2bb1fd6ca4600decbac47398aa5520a6
Robin Gustafsson <robin.gustafsson14@gmail.com>
2020-12-04 11:26:51 -0800

    Added basic Keypress to X11

--- src/platform/unix/Application_unix.cpp
+++ src/platform/unix/Application_unix.cpp
@@ -24,6 +24,38 @@ namespace pTK
     static Display *s_display = nullptr;
     static XContext s_xcontext = -1;
     static std::map<::Window, std::pair<Window*, MainWindow_unix*>> s_windowMap = {};
+    
+    static std::map<unsigned long, KeyCode> initKeyCodes() noexcept
+    {
+        std::map<unsigned long, KeyCode> map{};
+        map[XK_space] = Key::Space; map[XK_Escape] = Key::Escape;
+        map[XK_0] = Key::D0; map[XK_1] = Key::D1; map[XK_2] = Key::D2; map[XK_3] = Key::D3;
+        map[XK_4] = Key::D4; map[XK_5] = Key::D5; map[XK_6] = Key::D6; map[XK_7] = Key::D7;
+        map[XK_8] = Key::D8; map[XK_9] = Key::D9;
+        map[XK_KP_0] = Key::D0; map[XK_KP_1] = Key::D1; map[XK_KP_2] = Key::D2;
+        map[XK_KP_3] = Key::D3; map[XK_KP_4] = Key::D4; map[XK_KP_5] = Key::D5;
+        map[XK_KP_6] = Key::D6; map[XK_KP_7] = Key::D7; map[XK_KP_8] = Key::D8;
+        map[XK_KP_9] = Key::D9;
+        map[XK_a] = Key::A; map[XK_b] = Key::B; map[XK_c] = Key::C; map[XK_d] = Key::D;
+        map[XK_e] = Key::E; map[XK_f] = Key::F; map[XK_g] = Key::G; map[XK_h] = Key::H;
+        map[XK_i] = Key::I; map[XK_j] = Key::J; map[XK_k] = Key::K; map[XK_l] = Key::L;
+        map[XK_m] = Key::M; map[XK_n] = Key::N; map[XK_o] = Key::O; map[XK_p] = Key::P;
+        map[XK_q] = Key::Q; map[XK_r] = Key::R; map[XK_s] = Key::S; map[XK_t] = Key::T;
+        map[XK_u] = Key::U; map[XK_v] = Key::V; map[XK_w] = Key::W; map[XK_x] = Key::X;
+        map[XK_y] = Key::Y; map[XK_z] = Key::Z;
+        return map;
+    }
+
+    static const std::map<unsigned long, KeyCode> s_keyMap{initKeyCodes()};
+    
+    static Key translateKeyCode(unsigned long code)
+    {
+        std::map<unsigned long, KeyCode>::const_iterator it{s_keyMap.find(code)};
+        if (it != s_keyMap.cend())
+            return it->second;
+        
+        return Key::Unknown;
+    }
 
     Application_unix::Application_unix()
         : ApplicationBase(),
@@ -105,7 +137,6 @@ namespace pTK
             }
             case ClientMessage:
             {
-                PTK_INFO("TEST SADSAD");
                 if (auto uWindow = dynamic_cast<MainWindow_unix*>(window->getBackend()))
                 {
                     XClientMessageEvent *cEvent = reinterpret_cast<XClientMessageEvent*>(&event);
@@ -116,9 +147,10 @@ namespace pTK
                 }
                 break;
             }
-            case ButtonPress:
+            case KeyPress:
             {
-                
+                KeyEvent evt{KeyEvent::Pressed, translateKeyCode(XLookupKeysym(&event.xkey, 0))};
+                window->sendEvent(&evt);
                 break;
             }
             default:
