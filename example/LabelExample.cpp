234f3366890893995dcada9fcf6428ce4148a219
Robin Gustafsson <robin.gustafsson14@gmail.com>
2021-04-05 13:51:58 +0200

    Improved examples

--- example/LabelExample.cpp
+++ example/LabelExample.cpp
@@ -1,31 +1,41 @@
-#include "ptk/Application.hpp"
-#include "ptk/widgets/Label.hpp"
-
+#include "ptk/ptk.hpp"
 #include <iostream>
-
-// settings
-const unsigned int SCR_WIDTH = 960;
-const unsigned int SCR_HEIGHT = 540;
 
 int main(int argc, char *argv[])
 {
+    // Create app and window.
     pTK::Application app("LabelExample Application", argc, argv);
-    pTK::Window window("LabelExample Window", {SCR_WIDTH, SCR_HEIGHT});
-    
+    pTK::Window window("LabelExample Window", {960, 540});
+
+    // Create label.
     pTK::Ref<pTK::Label> label = pTK::Create<pTK::Label>();
+
+    // Name is NOT the displayed text.
+    // This is something optional to set, it can be used to identify the label.
     label->setName("My Label");
+
+    // Set font information.
     label->setFontSize(48);
     label->setFontFamily("Arial");
+
+    // Set displayed text.
     label->setText("Example label");
+
+    // Set colors and outline thickness.
     label->setColor(pTK::Color(0x161B21FF));
     label->setOutlineThickness(1.5f);
     label->setOutlineColor(pTK::Color(0xF4A950FF));
-    window.add(label);
+
+    // Callback for when the label is clicked.
     label->onClick([&label](pTK::Mouse::Button, const pTK::Point&){
         std::cout << label->getName() << " clicked!\n";
         return true;
     });
-    
+
+    // Add the label to the window.
+    window.add(label);
+
+
     return app.exec(&window);
 }
 
--- example/RectangleExample.cpp
+++ example/RectangleExample.cpp
@@ -1,30 +1,37 @@
-#include "ptk/Application.hpp"
-#include "ptk/widgets/Rectangle.hpp"
-
+#include "ptk/ptk.hpp"
 #include <iostream>
-
-// settings
-const unsigned int SCR_WIDTH = 960;
-const unsigned int SCR_HEIGHT = 540;
 
 int main(int argc, char *argv[])
 {
+    // Create app and window.
     pTK::Application app("RectangleExample Application", argc, argv);
-    pTK::Window window("RectangleExample Window", {SCR_WIDTH, SCR_HEIGHT});
-    
+    pTK::Window window("RectangleExample Window", {960, 540});
+
+    // Create rectangle.
     pTK::Ref<pTK::Rectangle> rect = pTK::Create<pTK::Rectangle>();
+
+    // This is something optional to set, it can be used to identify the rectangle.
     rect->setName("My Rectangle");
+
+    // Set size, position amd corner radius.
     rect->setSize(pTK::Size(250, 100));
     rect->setPosHint(pTK::Point(250, 250));
     rect->setCornerRadius(10);
+
+    // Set colors and outline thickness.
     rect->setColor(pTK::Color(0x161B21FF));
     rect->setOutlineThickness(2);
     rect->setOutlineColor(pTK::Color(0xF4A950FF));
-    window.add(rect);
+
+    // Callback for when the rectangle is clicked.
     rect->onClick([&rect](pTK::Mouse::Button, const pTK::Point&){
         std::cout << rect->getName() << " clicked!\n";
         return true;
     });
-    
+
+    // Add the label to the window.
+    window.add(rect);
+
+    // Run the application.
     return app.exec(&window);
 }
