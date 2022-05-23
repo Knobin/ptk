f4cbe37e729e2a8e8ff303eaef9c8d2b33e960ae
Robin Gustafsson <robin.gustafsson14@gmail.com>
2022-05-23 03:20:36 +0200

    Removed unused code in WidgetTests

--- tests/WidgetTest.cpp
+++ tests/WidgetTest.cpp
@@ -7,7 +7,7 @@
     All these tests are performed without a parent present.
     Parent will decide its child Size and Parent dynamically and
     if hard to test.
- 
+
     More tests will be added in the future to ensure that the Widget class
     is working correctly.
  */
@@ -15,7 +15,7 @@
 TEST_CASE("Constructors")
 {
     // Testing Constructors with correct data.
-    
+
     SECTION("Widget()")
     {
         pTK::Widget t;
@@ -31,21 +31,21 @@ TEST_CASE("Getters and Setters")
     // Testing Setters.
     pTK::Size cSize{10, 20};
     pTK::Point cPos{55, 85};
-    
+
     SECTION("setSize(const Size& size)")
     {
         pTK::Widget t;
         t.setSize(cSize);
         REQUIRE(t.getSize() == cSize);
     }
-    
+
     SECTION("setPosHint(const Position& pos)")
     {
         pTK::Widget t;
         t.setPosHint(cPos);
         REQUIRE(t.getPosition() == cPos);
     }
-    
+
     SECTION("Name")
     {
         pTK::Widget t;
@@ -57,28 +57,8 @@ TEST_CASE("Getters and Setters")
 TEST_CASE("Copy and Assignment")
 {
     // Testing Widget Copy and Assignment.
-    pTK::Size cSize{145, 235};
-    pTK::Size cMinSize{25, 55};
-    pTK::Size cMaxSize{95, 35};
-    pTK::Point cPos{430, 1050};
-    pTK::Widget t;
-    t.setSize(cSize);
-    t.setPosHint(cPos);
-    
-    SECTION("Copy")
-    {
-        pTK::Widget tmp = t;
-        REQUIRE(t.getSize() == cSize);
-        REQUIRE(t.getPosition() == cPos);
-    }
-    
-    SECTION("Assignment")
-    {
-        pTK::Widget tmp;
-        tmp = t;
-        REQUIRE(t.getSize() == cSize);
-        REQUIRE(t.getPosition() == cPos);
-    }
+
+    // TODO: Add tests.
 }
 
 /**
@@ -97,7 +77,7 @@ TEST_CASE ("Comparison")
     pTK::Widget t4;
     t4.setPosHint({10, 20});
     t4.setSize({42, 14});
-    
+
     SECTION("Equal")
     {
         REQUIRE(t == t1);
@@ -107,7 +87,7 @@ TEST_CASE ("Comparison")
         REQUIRE_FALSE(t2 == t4);
         REQUIRE_FALSE(t3 == t4);
     }
-    
+
     SECTION("Not Equal")
     {
         REQUIRE(t != t2);
