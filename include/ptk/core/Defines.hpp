1ea55b638d7240679eced0e165ab78a8f5d2278b
Robin Gustafsson <robin.gustafsson14@gmail.com>
2022-06-02 12:27:05 +0200

    Added more preprocessor directives

--- include/ptk/core/Defines.hpp
+++ include/ptk/core/Defines.hpp
@@ -8,38 +8,76 @@
 #ifndef PTK_CORE_DEFINES_HPP
 #define PTK_CORE_DEFINES_HPP
 
-#ifdef PTK_DEBUG
-    #define PTK_ENABLE_ASSERT
+// Build type.
+#if !defined(PTK_DEBUG) && !defined(PTK_RELEASE)
+    #ifdef NDEBUG
+        #define PTK_RELEASE
+    #else
+        #define PTK_DEBUG
+    #endif
+#endif
+
+// Enable debug options.
+#if defined(PTK_DEBUG)
+    #if !defined(PTK_ENABLE_ASSERT)
+        #define PTK_ENABLE_ASSERT
+    #endif
+#endif
+
+// Check compiler.
+#if !defined(PTK_COMPILER_GCC) && defined(__GNUC__)
+    #define PTK_COMPILER_GCC
+#elif !defined(PTK_COMPILER_CLANG) && defined(__clang__)
+    #define PTK_COMPILER_CLANG
+#elif !defined(PTK_COMPILER_MSVC) && defined(_MSC_VER)
+    #define PTK_COMPILER_MSVC
+#endif
+
+// Shared library defines.
+#if !defined(PTK_API)
+    #if defined(PTK_SHARED)
+        #if defined(PTK_COMPILER_MSVC)
+            #if defined(PTK_EXPORTS)
+                #define PTK_API __declspec(dllexport)
+            #else
+                #define PTK_API __declspec(dllimport)
+            #endif
+        #else
+            #define PTK_API __attribute__((visibility("default")))
+        #endif
+    #else
+        #define PTK_API
+    #endif
 #endif
 
 // Unused parameter.
-#ifdef PTK_COMPILER_GCC
+#if defined(PTK_COMPILER_GCC)
     #define UNUSED(x) x __attribute__((unused))
-#elif PTK_COMPILER_CLANG
+#elif defined(PTK_COMPILER_CLANG)
     #define UNUSED(x) x __attribute__((unused))
-#elif PTK_COMPILER_MSVC
+#elif defined(PTK_COMPILER_MSVC)
     #define UNUSED(x) __pragma(warning(suppress:4100)) x
 #else
     #define UNUSED(x)
 #endif
 
 // Enable Asserts
-#ifdef PTK_ENABLE_ASSERT
+#if defined(PTK_ENABLE_ASSERT)
     #define PTK_ASSERT(x, ...)   if(!(x)) { PTK_ERROR("Assertion Failed: {0}", __VA_ARGS__); abort(); }
 #else
     #define PTK_ASSERT(...)
 #endif
 
 // Ignore all warnings (Begin)
-#ifndef PTK_DISABLE_WARN_BEGIN
-    #ifdef PTK_COMPILER_GCC
+#if !defined(PTK_DISABLE_WARN_BEGIN)
+    #if defined(PTK_COMPILER_GCC)
         // TODO
         #define PTK_DISABLE_WARN_BEGIN()
-    #elif PTK_COMPILER_CLANG
+    #elif defined(PTK_COMPILER_CLANG)
         #define PTK_DISABLE_WARN_BEGIN() \
             _Pragma("clang diagnostic push") \
             _Pragma("clang diagnostic ignored \"-Weverything\"")
-    #elif PTK_COMPILER_MSVC
+    #elif defined(PTK_COMPILER_MSVC)
         #define PTK_DISABLE_WARN_BEGIN() __pragma(warning(push, 0))
     #else
         #define PTK_DISABLE_WARN_BEGIN()
@@ -47,13 +85,13 @@
 #endif
 
 // Ignore all warnings (End)
-#ifndef PTK_DISABLE_WARN_END
-    #ifdef PTK_COMPILER_GCC
+#if !defined(PTK_DISABLE_WARN_END)
+    #if defined(PTK_COMPILER_GCC)
         // TODO
         #define PTK_DISABLE_WARN_END()
-    #elif PTK_COMPILER_CLANG
+    #elif defined(PTK_COMPILER_CLANG)
         #define PTK_DISABLE_WARN_END() _Pragma("clang diagnostic pop");
-    #elif PTK_COMPILER_MSVC
+    #elif defined(PTK_COMPILER_MSVC)
         #define PTK_DISABLE_WARN_END() __pragma(warning(pop))
     #else
         #define PTK_DISABLE_WARN_END()
